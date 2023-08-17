7aaff5218ae394831f23c86ecf773a8828ee55b8
Simon Wittum <simonwittum@gmx.de>
2023-08-17 15:00:39 +0200

    update

--- aqt_main.py
+++ aqt_main.py
@@ -19,20 +19,14 @@ derivative = EuropeanOption(underlier, strike=1.1, maturity=maturity)
 
 # TODO: Find way to read in fit parameters from jax trained network
 backup_data = torch.load('model_weights.pth')
+backup_data['quantum.params'] = backup_data.pop('quantum.weights')
 
 
 criterion = ExpectedShortfall(0.1)
 model = AQT_NN(n_qubits, n_layers, in_features)
-model.load_state_dict(backup_data)
+# model.load_state_dict(backup_data)
 hedger = Hedger(model, ["log_moneyness", "time_to_maturity", "volatility"], criterion)
 
 
-print(model.state_dict())
-
-
-
-
-
-
-# pnl = criterion(hedger.compute_pnl(derivative, n_paths=10)).item()
-# print(pnl)
+pnl = criterion(hedger.compute_pnl(derivative, n_paths=10)).item()
+print(pnl)
